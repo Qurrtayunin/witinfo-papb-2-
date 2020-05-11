2abc4b7d36adc70acf66736d607500f0398442c0
babybreath101 <annastasyafar04@gmail.com>
2020-05-12 03:10:28 +0700

    commit localization

---
+++ app/src/main/java/com/example/witinfo/ui/detail_artikel/DetailArtikel.java
@@ -0,0 +1,21 @@
+package com.example.witinfo.ui.detail_artikel;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.witinfo.R;
+
+public class DetailArtikel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_tanaman);
+
+        getSupportActionBar().hide();
+
+
+    }
+}
+
